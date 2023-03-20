c4fddc133b2fb8241aaf9e724f954282acdad31e
GitKaspar <127048948+GitKaspar@users.noreply.github.com>
2023-03-20 05:25:17 +0200

    Update Peamine.java

--- Põhiprogramm/Peamine.java
+++ Põhiprogramm/Peamine.java
@@ -1,31 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.io.File;
 import java.util.Scanner;
 
 public class Peamine {
     // Erindi viskamiseta ei saa failist lugeda - veateade.
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 
-        Scanner tutvustus = new Scanner(new File ("šamaani tutvustus.txt"), "UTF-8");
-        while(tutvustus.hasNextLine()){
+        System.out.println("Teretulemast mängima meie mängu \"Draakon\"!");
+        System.out.println("Tekst kuvatakse mängijale iseenesest.");
+        System.out.println("Kui ilmuvad kolm punkti (...), kliki järgmisel real ja vajuta jätkamiseks \"Enter\".");
+        System.out.println("Valiku kinnitamiseks kliki mänguaknas, sisesta valiku number ja vajuta \"Enter\".");
+        System.out.println("Toredat mängimist!");
+        System.out.println();
+
+        Scanner tutvustus = new Scanner(new File("šamaani tutvustus.txt"), "UTF-8");
+        while (tutvustus.hasNextLine()) {
             String rida = tutvustus.nextLine();
             int reapikkus;
             if (rida.isEmpty())
-                reapikkus = 0; // vaikimisi 50
+                reapikkus = 50; // hilisemate valikute testimiseks sea väärtuseks 0
             else
-                reapikkus = 0; // esialgu rida.length()
+                reapikkus = rida.length(); // hilisemate valikute testimiseks sea väärtuseks 0
             väljastaPausiga(rida, reapikkus / 15); // vaikimisi 15 hetkel parim
         }
 
         // Loeme failist kohad
         Scanner kohad = new Scanner(new File("kohad.txt"), "UTF-8");
         List<Koht> kohaList = new ArrayList<>();
-        while(kohad.hasNextLine()){
+        while (kohad.hasNextLine()) {
             String rida = kohad.nextLine();
             String[] tükeldatudRida = rida.split(";");
-            Koht uusKoht =new Koht(tükeldatudRida[0], Boolean.parseBoolean(tükeldatudRida[1]));
+            Koht uusKoht = new Koht(tükeldatudRida[0], Boolean.parseBoolean(tükeldatudRida[1]));
             kohaList.add(uusKoht);
         }
 
@@ -33,44 +41,47 @@ public class Peamine {
         väljastaPausiga("Vana astub oiates oma telki.", 1);
         väljastaPausiga("Vaatad veidi kahvatus külas ringi ja sead end minekuks valmis.", 1);
         väljastaPausiga("Ülesande täitmist võid alustada sealt, kust soovid.", 1);
-        väljastaPausiga("Kuhu kõigepealt? (Vali, sisestades järgmisele reale 1 - 5.)" , 1);
+        väljastaPausiga("Kuhu kõigepealt?", 1);
         System.out.println();
 
         int jrkLuger = 1;
-        for (Koht koht:
-             kohaList) {
+        for (Koht koht :
+                kohaList) {
             System.out.println(jrkLuger++ + ") " + koht);
         }
 
         valik(kohaList);
 
     }
+
     // Kuna lause ja paus kordusid tihti, lõin selleks eraldi meetodi.
-    public static void väljastaPausiga (String lause, int sekundeid) throws Exception{
+    public static void väljastaPausiga(String lause, int sekundeid) throws Exception {
         int millisekundid = sekundeid * 1000; // Annab sekundile vastava suuruse
         System.out.println(lause);
-        Thread.sleep(millisekundid);
+        if (lause.isEmpty()) {
+            BufferedReader jätkaLugemist = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("...");
+            jätkaLugemist.read();
+        } else
+            Thread.sleep(millisekundid);
     }
 
-    public static void valik(List<Koht> kohaList){
+    public static void valik(List<Koht> kohaList) {
 
         Scanner valikuNr = new Scanner(System.in);
-        System.out.print("Palun sisesta number: ");
+        System.out.print("Palun sisesta number ja vajuta \"Enter\": ");
         int sisestatudNumber = valikuNr.nextInt();
 
-        switch (sisestatudNumber){
-            case 1: System.out.println("Sinu valik: 1. " + kohaList.get(0));
-                break;
-            case 2: System.out.println("Sinu valik: 2. " + kohaList.get(1));
-                break;
-            case 3: System.out.println("Sinu valik: 3. " + kohaList.get(2));
-                break;
-            case 4: System.out.println("Sinu valik: 4. " + kohaList.get(3));
-                break;
-            case 5: System.out.println("Sinu valik: 5. " + kohaList.get(4));
-                break;
-            default: {System.out.println("Vahemikus 1-5, palun.");
-                valik(kohaList);}
+        switch (sisestatudNumber) {
+            case 1 -> System.out.println("Sinu valik: 1. " + kohaList.get(0));
+            case 2 -> System.out.println("Sinu valik: 2. " + kohaList.get(1));
+            case 3 -> System.out.println("Sinu valik: 3. " + kohaList.get(2));
+            case 4 -> System.out.println("Sinu valik: 4. " + kohaList.get(3));
+            case 5 -> System.out.println("Sinu valik: 5. " + kohaList.get(4));
+            default -> {
+                System.out.println("Vahemikus 1-5, palun.");
+                valik(kohaList);
+            }
+        }
     }
-}
 }
