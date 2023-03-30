0d5bbbfd12f8fed3a7a90e479ee2d09ded43f590
GitKaspar <127048948+GitKaspar@users.noreply.github.com>
2023-03-30 05:09:11 +0300

    Update Koht.java

--- Alfa/Koht.java
+++ Alfa/Koht.java
@@ -1,10 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Koht {
         private String nimi;
         private boolean kasKülastatud;
+        private boolean kasÕnnestus;
+        private List<String> õnnestumine = new ArrayList<>();
+        private List<String> ebaõnnestumine = new ArrayList<>();
+        private String tulemusteFail;
 
-    public Koht(String nimi, boolean kasKülastatud) {
+
+    public Koht(String nimi, boolean kasKülastatud, String tulemusteFail) {
         this.nimi = nimi;
         this.kasKülastatud = kasKülastatud;
+        this.tulemusteFail = tulemusteFail;
+        this.kasÕnnestus = false;
     }
 
     @Override
@@ -13,6 +27,47 @@ public class Koht {
                 return nimi + ". Oled siin käinud.";
             else
                 return nimi + ". Sa ei ole siin käinud.";
+    }
+
+    public boolean isKasKülastatud() {
+        return kasKülastatud;
+    }
+
+    public void setKasKülastatud(boolean kasKülastatud) {
+        this.kasKülastatud = kasKülastatud;
+    }
+
+    public boolean isKasÕnnestus() {
+        return kasÕnnestus;
+    }
+
+    public void setKasÕnnestus(boolean kasÕnnestus) {
+        this.kasÕnnestus = kasÕnnestus;
+    }
+
+    public void loeTulemused() throws FileNotFoundException {
+        Scanner loeFailist = new Scanner(new File(this.tulemusteFail));
+        while (loeFailist.hasNextLine()){
+            String rida = loeFailist.nextLine();
+            if (rida.startsWith("T."))
+                    this.õnnestumine.add(rida.substring(2));
+            else
+                this.ebaõnnestumine.add(rida);
         }
+    }
+
+    public void väljastaTulemus() throws Exception {
+        if (this.kasÕnnestus)
+            for (String rida:
+                 this.õnnestumine) {
+                Abi.väljastaPausiga(rida);
+            }
+        else{
+            for (String rida:
+                 this.ebaõnnestumine) {
+                Abi.väljastaPausiga(rida);
+            }
+        }
+
     }
 }
